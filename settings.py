f89927ba5b910704df7864c92297e271205ec9cf
CarloLopez <86852232+CarloLopez@users.noreply.github.com>
2023-05-14 16:14:15 +0100

    Update ship movement

--- settings.py
+++ settings.py
@@ -3,7 +3,12 @@ class Settings:
 
     def __init__(self):
         """Initialise the game's settings."""
+
         # Screen settings
+        self.fullscreen = False
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (46, 23, 82)
+
+        # Ship settings
+        self.ship_speed = 1.5
